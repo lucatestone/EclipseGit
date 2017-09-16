db09dede714c0f88f445d19cd350f76a5f83c861
bighed <lucatestone@gmail.com>
2017-09-16 23:30:38 +0200

    Unary operators

---
+++ OCA/src/com/chapter2/Operators.java
@@ -0,0 +1,28 @@
+package com.chapter2;
+
+public class Operators {
+
+	public static void main(String[] args) {
+		
+		int counter = 0;
+		System.out.println("Valore iniziale del counter: " + counter);
+		
+		System.out.println("counter++: " + counter++);
+		
+		System.out.println("Senza aver fatto modifiche il counter vale: " + counter);
+		
+		System.out.println("++counter: " + ++counter);
+		
+		System.out.println("++counter * counter++ " + ++counter * counter++);
+		
+		
+		/*
+		int x = 3;
+		int y = ++x * 5 / --x + --x;
+		
+		System.out.println(y);
+		*/
+		
+	}
+
+}
